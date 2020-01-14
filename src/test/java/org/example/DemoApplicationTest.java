bfa00b2928e48c36b07e5112c2cfb988e223a387
Daniel e Kowalik <daniel@kowalik.org.pl>
2020-01-14 09:20:10 +0100

    plus IT tests

--- src/test/java/org/example/DemoApplicationTest.java
+++ src/test/java/org/example/DemoApplicationTest.java
@@ -1,12 +1,21 @@
 package org.example;
 
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class DemoApplicationTest {
 
+    @Autowired
+    ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertThat(applicationContext).isNotNull();
     }
 }
