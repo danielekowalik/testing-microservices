fef494338ee673f2aa67d878c44c57351c3cc018
Daniel e Kowalik <daniel@kowalik.org.pl>
2020-01-15 19:41:04 +0100

    pets api

--- src/main/java/org/example/controller/PetsApiControllerImpl.java
+++ src/main/java/org/example/controller/PetsApiControllerImpl.java
@@ -1,6 +1,7 @@
 package org.example.controller;
 
 import com.demo.mock.server.registration.api.PetsApi;
+import com.demo.mock.server.registration.model.Pet;
 import com.demo.mock.server.registration.model.Pets;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-public class PetsApiController implements PetsApi {
+public class PetsApiControllerImpl implements PetsApi {
 
     @Override
     public ResponseEntity<Void> createPets() {
@@ -17,11 +18,23 @@ public class PetsApiController implements PetsApi {
 
     @Override
     public ResponseEntity<Pets> listPets(@Valid Integer limit) {
-        return null;
+        Pets pets = new Pets();
+        Pet pet = new Pet();
+        pet.setId(12L);
+        pet.setName("My Cat");
+        pet.setTag("weird one");
+        pets.add(pet);
+        return ResponseEntity.ok(pets);
     }
 
     @Override
     public ResponseEntity<Pets> showPetById(String petId) {
-        return null;
+        Pets pets = new Pets();
+        Pet pet = new Pet();
+        pet.setId(Long.valueOf(petId));
+        pet.setName("My Cat");
+        pet.setTag("weird one");
+        pets.add(pet);
+        return ResponseEntity.ok(pets);
     }
 }
